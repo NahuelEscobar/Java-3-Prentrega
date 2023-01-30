0a1ee8f7d42fb4abb3516d7fd40bbf66a236d8a0
Nahuel Escobar <nahue.escobar.1996@gmail.com>
2023-01-30 16:21:03 -0300

    3 preentrega

---
+++ stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+  {id: 1, nombre: "Buzo Estampado", tipo: "buzo", cantidad: 1, desc: "Buzo ", precio: 5400, talle: "UNICO", img: './img/buzotigre.jpg'},
+  {id: 2, nombre: "Remera Anisha ", tipo: "remera1", cantidad: 1, desc: "Remera Rojo", precio: 2500, talle: "UNICO", img: './img/remeraanisha.jpg'},
+  {id: 3, nombre: "Remera Trebol", tipo: "remera2", cantidad: 1, desc: "Remera Blanca", precio: 3200, talle: "UNICO", img: './img/remeratrebol.jpg'},
+  {id: 4, nombre: "Remera Auntuan", tipo: "remera3", cantidad: 1, desc: "Remera Negra", precio: 2300, talle: "UNICO", img: './img/remeraantuan.jpg'},
+  {id: 5, nombre: "Remera Bulls", tipo: "remera4", cantidad: 1, desc: "Remera Blancat", precio: 1800, talle: "UNICO", img: './img/remerabulls.jpg'},
+  {id: 6, nombre: "Remera Cebra", tipo: "remera5", cantidad: 1, desc: "Remera Blaca/Negra", precio: 3200, talle: "UNICO", img: './img/remeracebra.jpeg'},
+  {id: 7, nombre: "Remera Fini", tipo: "remera6", cantidad: 1, desc: "Remera Negra", precio: 2500, talle: "UNICO", img: './img/remerafini.jpg'},
+  {id: 8, nombre: "Remera Mini Pink", tipo: "remera7", cantidad: 1, desc: "Remera Rosa", precio: 3500, talle: "UNICO", img: './img/remeraminipink.jpg'},
+  {id: 9, nombre: "Remera New york", tipo: "remera8", cantidad: 1, desc: "Remera Blanca", precio: 2100, talle: "Unico", img: './img/remeranewyork.jpg'},
+]
